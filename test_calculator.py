07e6b2e11ebed3bbd482429da953b8ae4d9eb01f
jahidul-arafat <jahidapon@gmail.com>
2022-02-11 18:45:25 +0600

    test fail

--- test_calculator.py
+++ test_calculator.py
@@ -13,4 +13,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 101 == calculator.multiply(10, 10)
+        assert 102 == calculator.multiply(10, 10)
