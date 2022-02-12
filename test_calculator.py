e8eaa55fb871038ee0c603b35862eb037439ba5b
jahidul-arafat <jahidapon@gmail.com>
2022-02-12 11:35:43 +0600

    test_multiplication corrected

--- test_calculator.py
+++ test_calculator.py
@@ -13,4 +13,4 @@ class TestCalculator:
         assert 2 == calculator.subtract(4, 2)
 
     def test_multiplication(self):
-        assert 102 == calculator.multiply(10, 10)
+        assert 100 == calculator.multiply(10, 10)
